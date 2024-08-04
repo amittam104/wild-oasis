8946a71c270c444a5fcd0b14f77887e08c9baf6b
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-04 12:24:36 +0530

    chore: add styled components, setup heading, Button and Input styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  margin: 4rem;
+`;
+
 function App() {
-  return <div>Hello Wild Oasis</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button size="small" variation="secondary">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Your Info</Heading>
+            <div>
+              <Input placeholder="Enter a number"></Input>
+              <Input placeholder="Enter a number"></Input>
+            </div>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  background-color: var(--color-grey-50);
+  border: 2px solid var(--color-grey-200);
+  border-radius: var(--border-radius-md);
+  font-size: 1.8;
+  padding: 1rem 2rem;
+`;
+
+export default Input;
