ec70c39106ddea15b540855ebeda7ec85adee110
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-05 07:26:26 +0530

    chore: connect supabase client, test fetch cabin data

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  let { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Could not get the cabins. Please try again!");
+  }
+
+  return data;
+}
