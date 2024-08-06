84426692efa985966f461ac33dfa5b9e4ba5a7d7
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-06 11:27:15 +0530

    chore: fecth cabins and display in a table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +40,26 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const {
+    name,
+    maxCapacity: capacity,
+    regularPrice: price,
+    discount,
+    image,
+  } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={image}></Img>
+      <Cabin>{name}</Cabin>
+      <div>Fits upto {capacity} guests</div>
+      <Price>{formatCurrency(price)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
