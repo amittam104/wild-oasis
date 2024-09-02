83fb11d62e41fc51beaed64e28804b26acc5f177
amittambulkar104 <amittambulkar104@gmail.com>
2024-09-02 12:11:37 +0000

    add sort on cabins table

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,6 +1,31 @@
+import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
+import TableOperations from "../../ui/TableOperations";
+
 /* eslint-disable react/prop-types */
-function CabinTableOperations({ children }) {
-  return <div>{children}</div>;
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No Discount" },
+          { value: "with-discount", label: "With Discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort By Name (A-Z)" },
+          { value: "name-desc", label: "Sort By Name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort By Price (low first)" },
+          { value: "regularPrice-desc", label: "Sort By Price (high first)" },
+          { value: "maxCapacity-asc", label: "Sort By Capacity (low first)" },
+          { value: "maxCapacity-desc", label: "Sort By Capacity (high first)" },
+        ]}
+      />
+    </TableOperations>
+  );
 }
 
 export default CabinTableOperations;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,23 +3,13 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
 import CabinTableOperations from "../features/cabins/CabinTableOperations";
-import Filter from "../ui/Filter";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <CabinTableOperations>
-          <Filter
-            filterField="discount"
-            options={[
-              { value: "all", label: "All" },
-              { value: "no-discount", label: "No Discount" },
-              { value: "with-discount", label: "With Discount" },
-            ]}
-          />
-        </CabinTableOperations>
+        <CabinTableOperations />
       </Row>
 
       <Row>
