baacb8123bf7e3f02b16f50686759816c9235fb4
amittambulkar104 <amittambulkar104@gmail.com>
2024-09-09 11:15:53 +0000

    chore: add user logout function and fix a login bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,18 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export default function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
-    onSuccess: (data) => {
-      console.log(data);
-      navigate("/dashboard");
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
       toast.success("You are successfully logged in");
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (err) => {
